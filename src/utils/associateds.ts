cdbb42c5e0cef68bdb8a437e63a06fe9bba4508a
Carlos Jr <rs.carlosjunior@gmail.com>
2025-03-14 01:05:49 -0300

    adicionando lista de associados

--- src/utils/associateds.ts
+++ src/utils/associateds.ts
@@ -2,58 +2,112 @@ export const associatedsGroup = [
   {
     responsibleId: "5527988534645",
     keyword: "Ilha Cinnabar",
-  },
-  {
-    responsibleId: "556281398131",
-    keyword: "Arena",
+    isPremium: true,
   },
   {
     responsibleId: "5527988534645",
     keyword: "STAFF",
+    isPremium: false,
+  },
+  {
+    responsibleId: "5527988534645",
+    keyword: "magikarp",
+    isPremium: false,
+  },
+  {
+    responsibleId: "5516997811602",
+    keyword: "lucario",
+    isPremium: false,
+  },
+  {
+    responsibleId: "5521971178880",
+    keyword: "Aliança",
+    isPremium: false,
+  },
+  {
+    responsibleId: "556281398131",
+    keyword: "Arena",
+    isPremium: false,
+  },
+  {
+    responsibleId: "5511933396550",
+    keyword: "NICOTCG",
+    isPremium: false,
+  },
+  {
+    responsibleId: "5517988322708",
+    keyword: "Pokécenter",
+    isPremium: false,
+  },
+  {
+    responsibleId: "5511970670479",
+    keyword: "Pokéttox",
+    isPremium: false,
+  },
+  {
+    responsibleId: "5519995000880",
+    keyword: "Temdetudogeek",
+    isPremium: false,
+  },
+  {
+    responsibleId: "5521982573467",
+    keyword: "MossLAX TCG",
+    isPremium: false,
+  },
+  {
+    responsibleId: "5519982544132",
+    keyword: "MewEevee",
+    isPremium: false,
+  },
+  {
+    responsibleId: "5511933396550",
+    keyword: "NICOTCG",
+    isPremium: false,
   },
   {
     responsibleId: "5511972115879",
     keyword: "pokéhub",
+    isPremium: false,
   },
   {
     responsibleId: "5513981494768",
-    keyword: "Vermilion",
+    keyword: "vermilion",
+    isPremium: false,
   },
   {
     responsibleId: "554284353780",
     keyword: "AuroraGeek",
+    isPremium: false,
   },
   {
     responsibleId: "5511989017858",
     keyword: "Sylveon",
-  },
-  {
-    responsibleId: "554588310430",
-    keyword: "AmourahTCG",
+    isPremium: false,
   },
   {
     responsibleId: "5521997031062",
     keyword: "pokêtop",
+    isPremium: false,
   },
   {
     responsibleId: "5527998351989",
     keyword: "Zapzone",
-  },
-  {
-    responsibleId: "5511985536495",
-    keyword: "POKECHAINA",
+    isPremium: false,
   },
   {
     responsibleId: "5517999754660",
     keyword: "TAKAHASHI",
+    isPremium: false,
   },
   {
     responsibleId: "5511983666238",
     keyword: "Poképobre",
+    isPremium: false,
   },
   {
     responsibleId: "5521988569043",
     keyword: "MorpheuTCG",
+    isPremium: false,
   },
 ];
 
